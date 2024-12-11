86e6c844e015b511159858f17a8014fd97e66a35
deepakpopclub <dpkvrm999@gmail.com>
2024-12-12 04:45:52 +0530

    add react router routes and tailwind css

---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const CHAPTERS = [
+    {
+        'id': 1,
+        'title': 'Introduction',
+        'slug': 'introduction',
+        'topics': [
+            'Introduction',
+            'What is React? And why would you use it?'
+        ]
+    },
+    {
+        'id': 21,
+        'slug': 'building-a-multi-page-spa-with-react-router',
+        'title': 'Building a Multi-Page SPA with React Router',
+        'topics': [
+            'Introduction',
+        ]
+    }
+]
